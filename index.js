2bdacb75acb33af96f55f4bf835daf9e24efe512
Xolmuxammad <iprogerr08@gmail.com>
2022-09-17 18:29:47 +0500

    delete unnecessary things

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ let f = document.querySelector('.block');
 let button = document.createElement('button');
 let input = document.querySelector('.input');
 let elem = document.createElement('li');
-let el = document.createElement('li');
 
 let arr = [];
 function form(){
