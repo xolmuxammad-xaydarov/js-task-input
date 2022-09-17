07c008bd553f9790a4da3c0cdfcf89dd4c9f55c7
Xolmuxammad <iprogerr08@gmail.com>
2022-09-17 12:53:21 +0500

    added new files

---
+++ index.js
@@ -0,0 +1,23 @@
+let f = document.querySelector('.block');
+let button = document.createElement('button');
+let input = document.querySelector('.input');
+let elem = document.createElement('li');
+let el = document.createElement('li');
+
+let arr = [];
+function form(){
+    elem.setAttribute('class', 'elemList')
+    document.body.appendChild(elem);
+    f.appendChild(button);
+    button.textContent = 'Button'
+}
+form();
+button.addEventListener('click',(e) => {
+    e.preventDefault();
+
+    if(input.value.length > 0){
+        arr.push(input.value) + ' ';
+    }
+    elem.textContent = arr;
+ });
+ console.log(arr);
